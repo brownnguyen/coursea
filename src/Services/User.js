5da0a7c7a60566e49a42449c7799f2f0f4f2dd13
Brown Nguyễn <brownnguyen@gmail.com>
2020-09-18 08:37:09 +0700

    fix__services

--- src/Services/User.js
+++ src/Services/User.js
@@ -3,14 +3,14 @@ import Axios from "axios";
 export default class UserServices {
     Login (data) {
         return Axios({
-            url: 'http://elearning0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap',
+            url: 'https://elearning0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap',
             method: "POST",
             data
         })
     }
     SignUp (data) {
         return Axios({
-            url:"http://elearning0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangKy",
+            url:"https://elearning0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangKy",
             method: "POST",
             data
         })
